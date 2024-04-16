b5c99515092d99169c66520b4f59f515cdd61be5
lsherman98 <122409681+lsherman98@users.noreply.github.com>
2024-04-16 01:29:37 -0400

    fixed api calls

--- frontend/src/store/features/poemSlice.js
+++ frontend/src/store/features/poemSlice.js
@@ -6,25 +6,25 @@ const initialState = {
 };
 
 export const getPoems = createAsyncThunk("poems/getPoems", () => {
-    return fetch("http://localhost:4000/poems")
+    return fetch("https://poetry-365.onrender.com/poems")
         .then((res) => res.json())
         .catch((err) => console.log(err));
 });
 
 export const getPoem = createAsyncThunk("poems/getPoem", (id) => {
-    return fetch(`http://localhost:4000/poems/${id}`)
+    return fetch(`https://poetry-365.onrender.com/poems/${id}`)
         .then((res) => res.json())
         .catch((err) => console.log(err));
 });
 
 export const getPoemByDay = createAsyncThunk("poems/getPoem", (day) => {
-    return fetch(`http://localhost:4000/poems/day/${day}`)
+    return fetch(`https://poetry-365.onrender.com/poems/day/${day}`)
         .then((res) => res.json())
         .catch((err) => console.log(err));
 });
 
 export const getDailyPoem = createAsyncThunk("poems/getDailyPoem" , () => {
-        return fetch(`http://localhost:4000/poems/today`)
+        return fetch(`https://poetry-365.onrender.com/poems/today`)
             .then((res) => res.json())
             .catch((err) => console.log(err));
 })
